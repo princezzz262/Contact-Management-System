3e31de9ade4e16c48466b05a0be9e6554ee39054
princezzz262 <76697723+princezzz262@users.noreply.github.com>
2024-05-08 01:35:57 +0000

    Started

---
+++ src/Contact Management Sys.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : CMS.cpp
+// Author      : Prince Mensah Nana / princezzz262@github
+// Version     :	1.0.0
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Interface.h"
+using namespace std;
+
+int main() {
+	Interface();
+	return 0;
+}
